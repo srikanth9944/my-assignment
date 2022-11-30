c3ab0f08d80325c23fadfe736bd3cc257f342ec3
srikanth9944 <119466448+srikanth9944@users.noreply.github.com>
2022-11-29 21:24:58 -0800

    Add files via upload

---
+++ twowheeler.java
@@ -0,0 +1,25 @@
+package day2;
+
+public class twowheeler{
+	int noOfwheels =4;
+	short noOfMirrors =2;
+	long chassisNumber =98765432101343L;
+	boolean isPunctured =true;
+	double runningkm =23.87;
+public static void main(String[] args) {
+	twowheeler bike =new twowheeler();
+	System.out.println("noOfWheels"+bike.noOfwheels);
+	System.out.println("noOfWheels"+bike.noOfwheels);
+	System.out.println("chassisNumber"+bike.chassisNumber);
+	System.out.println("isPunctured"+bike.isPunctured);
+	System.out.println("runningKM"+bike.runningkm);
+}
+	
+}
+     
+     
+     
+     
+
+	
+       
