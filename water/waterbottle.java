f9fe64b1c0e8499489d2e570e3ace55775c666ef
SRIKANTH.P <119466448+srikanth9944@users.noreply.github.com>
2022-12-03 16:14:49 +0530

    Add files via upload

---
+++ water/waterbottle.java
@@ -0,0 +1,20 @@
+package water;
+
+public class waterbottle {
+    int noOfcover =1;
+    boolean heatTolerant =true;
+    float waterCapacity =1.10f;
+    boolean plasticWaterbottle =true;
+    short waterbottleWight =2;
+    boolean coolTolerant =true;
+    
+public static void main(String[] args) {
+	waterbottle water =new waterbottle();
+	
+	System.out.println("noOfcover"+water.noOfcover);
+	System.out.println("heatTolerant+water.heatTolerant");
+	System.out.println("waterCapacity+water.waterCapacity");
+	System.err.println("plasticWaterbottle+water.plasticWaterbottle");
+	System.out.println("waterbottleWight+water.waterbottleWight");
+	System.out.println("coolTolerant+water.coolTolerant");
+}}
