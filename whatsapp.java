da70f5aadaf827c76044a342f09a6a557858ac2b
SRIKANTH.P <119466448+srikanth9944@users.noreply.github.com>
2022-12-03 16:08:54 +0530

    Add files via upload

---
+++ whatsapp.java
@@ -0,0 +1,51 @@
+package day3;
+
+import java.security.PublicKey;
+
+public class whatsapp {
+	public void voiceChat() {
+		System.out.println("voiceChat");
+		}
+    public void VedioCall() {
+    	System.out.println("vedioCall");
+    	}
+    public void AudioCall() {
+    	System.out.println("AudioCall");
+    	}
+    public void SendMessage() {
+    	System.out.println("SendMessage");
+    	}
+    public void SendPhoto() {
+    	System.out.println("SendMessage");
+    	}
+    public void StatusUpload() {
+		System.out.println("StatusUpload");
+	    }
+    public void SendVedio() {
+    	System.out.println("SendVedio");
+        }
+    public void payment() {
+    	System.out.println("payment");
+		}
+    
+    
+    public static void main(String[] args) {
+    	whatsapp myWhatsapp=new whatsapp();
+    	
+    	myWhatsapp.voiceChat();
+    	myWhatsapp.VedioCall();
+    	myWhatsapp.AudioCall();
+    	myWhatsapp.SendMessage();
+    	myWhatsapp.SendPhoto();
+    	myWhatsapp.StatusUpload();
+    	myWhatsapp.SendVedio();
+    	myWhatsapp.payment();
+    	
+    	
+    	
+    	 
+    	
+		
+	}
+	
+}
