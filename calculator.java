8b973a088e470a37d6818789aaa4e8d040905689
SRIKANTH.P <119466448+srikanth9944@users.noreply.github.com>
2022-12-03 16:17:48 +0530

    Add files via upload

---
+++ calculator.java
@@ -0,0 +1,17 @@
+package week1day3;
+
+public class calculator {
+	public void subTwoNumber() {
+		int a=20;
+		int b=10;
+		int c=a-b;
+		System.out.println(c);
+		}
+	public static void main(String[] args) {
+		calculator calc=new calculator();
+		calc.subTwoNumber();
+		
+	}
+   
+		   
+}
