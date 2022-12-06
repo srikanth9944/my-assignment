062bb2c0170992f08f74c96c9c3725b110e699b6
SRIKANTH.P <119466448+srikanth9944@users.noreply.github.com>
2022-12-06 11:01:14 +0530

    Add files via upload

---
+++ EmployeeDetails.java
@@ -0,0 +1,17 @@
+package week2day2;
+
+public class EmployeeDetails {
+   String EmployeeName= "srikanth";
+   String EmployeeAddress="chennai";
+   long MobileNum=99657036;
+   double EmployeeSalary=35555.45;
+  
+public static void main(String[] args) {
+	EmployeeDetails week2day2=new EmployeeDetails();
+	System.out.println("EmployeeName=" +week2day2.EmployeeName);
+	System.out.println("EmployeeAddress=" +week2day2.EmployeeAddress);
+	System.out.println("MobileNum=" +week2day2.MobileNum);
+	System.out.println("EmployeeSalary=" +week2day2.EmployeeSalary);
+}}
+
+   
